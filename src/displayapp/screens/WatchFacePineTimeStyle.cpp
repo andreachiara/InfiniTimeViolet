1f763de4306976977d36e6ffc8f7677ebe6ab18e
andreachiara <aki.andreachiara@gmail.com>
2022-05-07 20:48:46 +0200

    removed re-setting of colors

--- src/displayapp/screens/WatchFacePineTimeStyle.cpp
+++ src/displayapp/screens/WatchFacePineTimeStyle.cpp
@@ -530,13 +530,13 @@ void WatchFacePineTimeStyle::UpdateSelected(lv_obj_t* object, lv_event_t event) 
     }
     if (object == btnReset) {
       needle_colors[0] = LV_COLOR_WHITE;
-      settingsController.SetPTSColorTime(Controllers::Settings::Colors::Teal);
+      //settingsController.SetPTSColorTime(Controllers::Settings::Colors::Teal);
       lv_obj_set_style_local_text_color(timeDD1, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Convert(Controllers::Settings::Colors::Teal));
       lv_obj_set_style_local_text_color(timeDD2, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Convert(Controllers::Settings::Colors::Teal));
       lv_obj_set_style_local_text_color(timeAMPM, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Convert(Controllers::Settings::Colors::Teal));
-      settingsController.SetPTSColorBar(Controllers::Settings::Colors::Teal);
+      //settingsController.SetPTSColorBar(Controllers::Settings::Colors::Teal);
       lv_obj_set_style_local_bg_color(sidebar, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, Convert(Controllers::Settings::Colors::Teal));
-      settingsController.SetPTSColorBG(Controllers::Settings::Colors::Black);
+      //settingsController.SetPTSColorBG(Controllers::Settings::Colors::Black);
       lv_obj_set_style_local_bg_color(timebar, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, Convert(Controllers::Settings::Colors::Black));
     }
     if (object == btnRandom) {
