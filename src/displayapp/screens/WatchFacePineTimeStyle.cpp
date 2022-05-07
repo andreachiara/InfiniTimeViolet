7ad3463bc47f86cf25cc528c549f7729fc0b2519
andreachiara <aki.andreachiara@gmail.com>
2022-05-08 00:32:52 +0200

    Fixed: Missssed an s

--- src/displayapp/screens/WatchFacePineTimeStyle.cpp
+++ src/displayapp/screens/WatchFacePineTimeStyle.cpp
@@ -598,7 +598,7 @@ Pinetime::Controllers::Settings::Colors WatchFacePineTimeStyle::GetPrevious(Pine
   if (colorAsInt > 0) {
     prevColor = static_cast<Controllers::Settings::Colors>(colorAsInt - 1);
   } else {
-    prevColor = GetPrevious(Controller::Settings::Colors::NUMCOLOR);
+    prevColor = GetPrevious(Controllers::Settings::Colors::NUMCOLOR);
   }
   return prevColor;
 }
