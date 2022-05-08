5eb72a28a3d40b92f07ae17a9c4bcecad6784a01
andreachiara <aki.andreachiara@gmail.com>
2022-05-08 02:02:18 +0200

    updated systeminfo with fork info

--- src/displayapp/screens/SystemInfo.cpp
+++ src/displayapp/screens/SystemInfo.cpp
@@ -77,9 +77,9 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen1() {
   lv_obj_t* label = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_recolor(label, true);
   lv_label_set_text_fmt(label,
-                        "#FFFF00 InfiniTime#\n\n"
+                        "#AC15CC InfiniTimeViolet#\n\n"
                         "#808080 Version# %ld.%ld.%ld\n"
-                        "#808080 Short Ref# %s\n"
+                        "#008080 Short Ref# %s\n"
                         "#808080 Build date#\n"
                         "%s\n"
                         "%s\n\n"
@@ -269,10 +269,10 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen5() {
                            "under the terms of\n"
                            "the GNU General\n"
                            "Public License v3\n"
-                           "#808080 Source code#\n"
+                           "#AC15CC Source code#\n"
                            "#FFFF00 https://github.com/#\n"
-                           "#FFFF00 InfiniTimeOrg/#\n"
-                           "#FFFF00 InfiniTime#");
+                           "#FFFF00 andreachiara/#\n"
+                           "#FFFF00 InfiniTimeViolet#");
   lv_label_set_align(label, LV_LABEL_ALIGN_CENTER);
   lv_obj_align(label, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
   return std::make_unique<Screens::Label>(4, 5, app, label);
